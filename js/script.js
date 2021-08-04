9399024eb6423fa310a16d666c8e6d5ce6c5c669
Samueelx <gsammygitau@gmail.com>
2021-08-04 07:22:58 +0300

    compute and display response to user

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ Pizza.prototype.calculatePrice = function() {
     else if (this.size === "small") {
         sizePrice = 0.75 * sizePrice;
     }
-    console.log("point2");
+    //console.log("point2");
     
     //calculating price of toppings
     if (this.toppings.length == 1) {toppingPrice = 250;}
@@ -26,11 +26,19 @@ Pizza.prototype.calculatePrice = function() {
     return sizePrice + crustPrice + toppingPrice;
 }
 
+function isValid(testValue, min, max) {
+    return testValue < min || testValue > max;
+}
+
 
 
 $(document).ready( function() {
     $('.order').submit( function(event) {
         event.preventDefault();
+
+        let name = $('input#name').val();
+        let location = $('input#location').val();
+
         let size = "";
         let pizzaSize =  parseInt($('#pizza-size').val());
         if (pizzaSize === 1) size = "large";
@@ -43,18 +51,42 @@ $(document).ready( function() {
         let toppingList = $('input:checkbox[name = topping]:checked').map( function() {
             return this.value;
         });
-        console.log("point1");
+
         let toppings = [];
         for( let i = 0; i < toppingList.length; i++){
             toppings.push(toppingList[i]);
         }
-        console.log(toppings);
         //Now, topping is an array of the chosen toppings.
+
+
+        //Data Validation.
+        if (isValid(size, 1, 3)){
+            alert("Please enter the appropriate pizza size");
+        }
+        else if (isValid(toppings.length, 1, 4)) {
+            alert("You must select a pizza topping");
+        }
+        else if ((location = 0)) {
+            alert("Please enter your location");
+        }
+        
+
 
         //create the pizza object
         let order = new Pizza(size, crust, toppings);
-        console.log(order.size, order.crust, order.toppings);
-        alert(order.calculatePrice());
+        //console.log(order.size, order.crust, order.toppings);
+        output();
+        
+        function output() {
+            let congratulatoryText = $('<h3></h3>').text(`Thank you, ${name} for choosing us!`);
+            let confirm = $('<p></p>').text(`Your Pizza order has been collected and will be deliverer to you in less that 1 hr`);
+            let cost = $('<p></p>').text(`Your order will cost you, KSH ${order.calculatePrice()}`);
+
+            $('div#response').append(congratulatoryText);
+            $('div#response').append(confirm);
+            $('div#response').append(cost);
+
+        }
         
     });
 });
